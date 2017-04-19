49e73f883beb6f765087d9853e0b2c9dc9fe635f
Dan Esparza <esparza.dan@gmail.com>
2017-04-19 12:18:53 -0400

    Initial code commit
    Includes parsing flags, parsing tags, and logging to InfluxDB.  Smoke
    tested and working

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	"strings"
+
+	"github.com/influxdata/influxdb/client/v2"
+)
+
+func main() {
+	//	Our command line options:
+	influxServer := flag.String("server", "http://localhost:8086", "Full path to the influxdb server, including credentials")
+	influxDatabase := flag.String("database", "events", "InfluxDB database to write the annotation to")
+	measurementName := flag.String("name", "build", "Measurement name")
+	measurementValue := flag.String("value", "your_value_here", "Measurement value")
+	measurementTags := flag.String("tags", "", "Comma seperated list of key=value tags")
+	flag.Parse()
+
+	//	Influx write format:
+	//	build,application=Formbuilder value="FormBuilder-Build_20170417.1"
+	c, err := client.NewHTTPClient(client.HTTPConfig{Addr: *influxServer})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new point batch
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  *influxDatabase,
+		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//	Update our tags
+	tags := make(map[string]string)
+	kvPairs := strings.Split(*measurementTags, ",")
+
+	for _, kvPair := range kvPairs {
+		nameValue := strings.Split(kvPair, "=")
+		tags[nameValue[0]] = nameValue[1]
+	}
+
+	//	Update our measurement
+	measurements := map[string]interface{}{"value": *measurementValue}
+
+	// Create a point and add to batch
+	pt, err := client.NewPoint(*measurementName, tags, measurements, time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+	bp.AddPoint(pt)
+
+	// Write the batch
+	if err := c.Write(bp); err != nil {
+		log.Fatal(err)
+	}
+}
